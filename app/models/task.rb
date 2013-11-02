91d37d700b0146aedc388a7c20b05b0a0938946b
glenn0 <glenn0>
2013-11-02 15:26:35 +1100

    Initial commit, jQuery dragging with list.

---
+++ app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ActiveRecord::Base
+  attr_accessible :detail
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryReordering::Application.config.secret_token = '2821968995eec31e97a147383a1a079d25192a3ff337e7fe654de7b51ee85fb7d875a98df8f81d651985b357487cbc97d04f2d0c0543ab83f7a0c6b4c8710e0f'
